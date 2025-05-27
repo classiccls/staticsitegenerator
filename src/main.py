55f5a0f9db8b08aa087fbbd2d20dbb7d8d679c48
classiccls <classiccls@gmail.com>
2025-05-27 14:14:16 +0200

    wsio wparadie

--- src/main.py
+++ src/main.py
@@ -1,11 +1,26 @@
-from textnode import TextType
-from textnode import TextNode
-from htmlnode import HTMLNode
+import os
+import shutil
+
+from copystatic import copy_files_recursive
+from gencontent import generate_pages_recursive
+
+
+dir_path_static = "./static"
+dir_path_public = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
+
 
 def main():
-    new_text_node = TextNode("czapla", TextType.BOLD, "morda")
-    print(new_text_node)
-    new_html_node = HTMLNode("tag", "value", "children", "props")
-    print(new_html_node)
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
+
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
+
+    print("Generating content...")
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
+
 
 main()
